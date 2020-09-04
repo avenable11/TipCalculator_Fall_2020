ad020795de59d3b4190aab91a5db45be586d8bbf
Angela K Venable <avenable11@ivytech.edu>
2020-09-03 21:04:13 -0400

    Initial commit

---
+++ app/src/main/java/edu/ivytech/tipcalculatorfall2020/TipCalculatorActivity.java
@@ -0,0 +1,14 @@
+package edu.ivytech.tipcalculatorfall2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TipCalculatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tipcalculator);
+    }
+}
