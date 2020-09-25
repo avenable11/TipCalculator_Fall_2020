aa568b3c49a22b5185a4a9b8c792fc58c28feb9b
Angela K Venable <avenable11@ivytech.edu>
2020-09-24 21:19:40 -0400

    State after 9/24/2020 lecture

--- app/src/main/java/edu/ivytech/tipcalculatorfall2020/TipCalculatorActivity.java
+++ app/src/main/java/edu/ivytech/tipcalculatorfall2020/TipCalculatorActivity.java
@@ -7,9 +7,16 @@ import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
@@ -19,7 +26,11 @@ import static android.content.SharedPreferences.*;
 import static android.widget.TextView.*;
 
 
-public class TipCalculatorActivity extends AppCompatActivity {
+public class TipCalculatorActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
+
+    private final int ROUND_NONE = 0;
+    private final int ROUND_TIP = 1;
+    private final int ROUND_TOTAL = 2;
 
     private EditText billAmountEditText;
     private EditText tipEditText;
@@ -28,7 +39,15 @@ public class TipCalculatorActivity extends AppCompatActivity {
     private float tipPercent = .15f;
     private String billAmountString;
     private SharedPreferences savedValues;
-    private Spinner splitSpinner;
+    private AutoCompleteTextView splitSpinner;
+    private String[] s;
+    private ArrayAdapter<String> adapter;
+    private int rounding = ROUND_NONE;
+    private RadioGroup roundingRadioGroup;
+    private int split = 1;
+    private EditText eachPaysEditText;
+    private TextInputLayout eachPays;
+    private SeekBar tipSeekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,9 +70,45 @@ public class TipCalculatorActivity extends AppCompatActivity {
                 return false;
             }
         });
-
+        s = getResources().getStringArray(R.array.split_array);
+        splitSpinner = findViewById(R.id.splitBill);
+        adapter = new ArrayAdapter<String>(this, R.layout.dropdown_menu_popup_item,s);
+        splitSpinner.setAdapter(adapter);
+        splitSpinner.setText(s[0],false);
+        splitSpinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                split = position + 1;
+                calculateAndDisplay();
+            }
+        });
 
         savedValues = getSharedPreferences("SavedValues", MODE_PRIVATE);
+        eachPays = findViewById(R.id.eachPays);
+        roundingRadioGroup = findViewById(R.id.roundingRadioGroup);
+        roundingRadioGroup.setOnCheckedChangeListener(this);
+        roundingRadioGroup.check(R.id.noRoundingRadioButton);
+        //eachPays = findViewById(R.id.eachPays);
+        eachPaysEditText = findViewById(R.id.eachPaysEditText);
+        tipSeekBar = findViewById(R.id.tipSeekBar);
+        tipSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                percentEditText.setText(String.format("%d", progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                int progress = seekBar.getProgress();
+                tipPercent = (float) progress / 100;
+                calculateAndDisplay();
+            }
+        });
     }
 
     @Override
@@ -61,6 +116,8 @@ public class TipCalculatorActivity extends AppCompatActivity {
         Editor editor = savedValues.edit();
         editor.putString("billAmountString", billAmountString);
         editor.putFloat("tipPercent", tipPercent);
+        editor.putInt("rounding", rounding);
+        editor.putInt("split", split);
         editor.apply();
         super.onPause();
     }
@@ -71,6 +128,21 @@ public class TipCalculatorActivity extends AppCompatActivity {
         billAmountString = savedValues.getString("billAmountString","");
         tipPercent = savedValues.getFloat("tipPercent", 0.15f);
         billAmountEditText.setText(billAmountString);
+        int tipProgress = (int) (tipPercent * 100);
+        tipSeekBar.setProgress(tipProgress);
+        rounding = savedValues.getInt("rounding", ROUND_NONE);
+        if (rounding == ROUND_NONE) {
+            roundingRadioGroup.check(R.id.noRoundingRadioButton);
+        } else if (rounding == ROUND_TIP) {
+            roundingRadioGroup.check(R.id.roundTipRadioButton);
+        } else if (rounding == ROUND_TOTAL) {
+            roundingRadioGroup.check(R.id.roundTotalRadioButton);
+        }
+        split = savedValues.getInt("split", 1);
+        if (split > 1) {
+            eachPays.setVisibility(View.VISIBLE);
+        }
+        splitSpinner.setText(s[split - 1], false);
         calculateAndDisplay();
     }
 
@@ -83,8 +155,28 @@ public class TipCalculatorActivity extends AppCompatActivity {
             billAmount = Float.parseFloat(billAmountString);
         }
 
-        float tipAmount = billAmount * tipPercent;
-        float totalAmount = billAmount + tipAmount;
+        float tipAmount = 0;
+        float totalAmount = 0;
+        if (rounding == ROUND_NONE) {
+            tipAmount = billAmount * tipPercent;
+            totalAmount = billAmount + tipAmount;
+        } else if (rounding == ROUND_TIP) {
+            tipAmount = StrictMath.round(billAmount * tipPercent);
+            totalAmount = billAmount + tipAmount;
+        } else if (rounding == ROUND_TOTAL) {
+            tipAmount = billAmount * tipPercent;
+            totalAmount = StrictMath.round(billAmount + tipAmount);
+            tipAmount = totalAmount - billAmount;
+        }
+
+
+        if (split == 1) {
+            eachPays.setVisibility(View.GONE);
+
+        } else {
+            eachPays.setVisibility(View.VISIBLE);
+            float splitAmount = totalAmount / split;
+        }
 
         NumberFormat currency = NumberFormat.getCurrencyInstance();
         NumberFormat percent = NumberFormat.getPercentInstance();
@@ -93,6 +185,14 @@ public class TipCalculatorActivity extends AppCompatActivity {
         symbols.setPercent('\0');
         ((DecimalFormat) currency).setDecimalFormatSymbols(symbols);
         ((DecimalFormat) percent).setDecimalFormatSymbols(symbols);
+
+        if (split == 1) {
+            eachPays.setVisibility(View.GONE);
+        } else {
+            eachPays.setVisibility(View.VISIBLE);
+            float splitAmount = totalAmount / split;
+            eachPaysEditText.setText(currency.format(splitAmount));
+        }
 
         tipEditText.setText(currency.format(tipAmount));
         totalEditText.setText(currency.format(totalAmount));
@@ -104,6 +204,31 @@ public class TipCalculatorActivity extends AppCompatActivity {
             case R.id.calculateButton:
                 calculateAndDisplay();
                 break;
+            case R.id.clearButton:
+                billAmountEditText.setText("");
+                tipPercent = .15f;
+                percentEditText.setText(R.string.default_percent);
+                tipSeekBar.setProgress(15);
+                roundingRadioGroup.clearCheck();
+                rounding = ROUND_NONE;
+                roundingRadioGroup.check(R.id.noRoundingRadioButton);
+                tipEditText.setText(R.string.default_number);
+                splitSpinner.setText(s[0], false);
+                split = 1;
+                eachPaysEditText.setText(R.string.default_number);
+                eachPays.setVisibility(View.GONE);
+                totalEditText.setText(R.string.default_number);
         }
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        switch (checkedId) {
+            case R.id.noRoundingRadioButton: rounding = ROUND_NONE; break;
+            case R.id.roundTipRadioButton: rounding = ROUND_TIP; break;
+            case R.id.roundTotalRadioButton: rounding = ROUND_TOTAL; break;
+        }
+
+        calculateAndDisplay();
     }
 }
